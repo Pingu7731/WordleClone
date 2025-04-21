ad5eb891fbecf9cad713c25b767e2c111bba5b00
NootNoot <101384541+Pingu7731@users.noreply.github.com>
2025-04-21 22:15:10 +0800

    Tiles

---
+++ wordleclone/lib/wordle/data/word_list.dart
@@ -0,0 +1,11 @@
+const List<String> fiveLetterWords = [
+  'hello',
+  'tests',
+  'after',
+  'years',
+  'place',
+  'sound',
+  'again',
+  'still',
+  'think',
+];
--- wordleclone/lib/wordle/views/wordle_screen.dart
+++ wordleclone/lib/wordle/views/wordle_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:wordleclone/wordle/data/word_list.dart';
+import 'package:wordleclone/wordle/model/letter_model.dart';
+import 'package:wordleclone/wordle/model/word_model.dart';
+import 'package:wordleclone/wordle/widget/board.dart';
+
+enum GameStatus { playing, submitting, lost, won }
 
 class WordleScreen extends StatefulWidget {
   const WordleScreen({super.key});
@@ -8,8 +15,38 @@ class WordleScreen extends StatefulWidget {
 }
 
 class _WordleScreenState extends State<WordleScreen> {
+  GameStatus gameStatus = GameStatus.playing;
+
+  final List<Word> board = List.generate(
+    6,
+    (_) => Word(letters: List.generate(5, (_) => Letter.empty())),
+  );
+  int currentWordIndex = 0;
+
+  Word? get currentword =>
+      currentWordIndex < board.length ? board[currentWordIndex] : null;
+
+  Word solution = Word.fromString(
+    fiveLetterWords[Random().nextInt(fiveLetterWords.length)].toUpperCase(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'WORDLE',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 4,
+          ),
+        ),
+      ),
+      body: Column(children: [Board(board: [])]),
+    );
   }
 }
